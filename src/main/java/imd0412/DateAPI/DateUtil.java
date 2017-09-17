aea4394332079d077d3b6a85b6a7a610f2cb5790
Gabriel Araújo <gabriel140492@gmail.com>
2017-09-17 02:12:48 -0300

    Modifications on DataUtil class - Add private constructor - Comment out some unreachable code

--- src/main/java/imd0412/DateAPI/DateUtil.java
+++ src/main/java/imd0412/DateAPI/DateUtil.java
@@ -11,6 +11,9 @@ import java.util.Set;
  */
 public class DateUtil
 {
+
+	private DateUtil() {}
+
 	private static boolean has30(int month)
 	{
 		return months30.contains(month);
@@ -55,28 +58,30 @@ public class DateUtil
 
 		String result;
 
-		/* day is always greater equals than 1*/
-		if ((day >= 1 && day <= 27) || (has31(month) && day < 31) || (has30(month) && day < 30) || (isFebruary(month) && isLeapYear(year) && day == 28))
+		/* day is always greater equals than 1 at this point*/
+		if ((/*day >= 1 &&*/ day <= 27)
+				|| (has31(month) && day < 31)
+				|| (has30(month) && day < 30)
+				|| (isFebruary(month) && isLeapYear(year) && day == 28))
 		{
 			result = String.format(dateFormat, day + 1, month, year);
 		}
-		else if (
-				(has31(month) && month != DECEMBER && day == 31)
-						|| (has30(month) && day == 30)
-						|| (isFebruary(month) && isLeapYear(year) && day == 29)
-						|| (isFebruary(month) && !isLeapYear(year) && day == 28))
+		else if ((has31(month) && month != DECEMBER && day == 31)
+				|| (has30(month) && day == 30)
+				|| (isFebruary(month) && isLeapYear(year) && day == 29)
+				|| (isFebruary(month) && !isLeapYear(year) && day == 28))
 		{
 			result = String.format(dateFormat, 1, month + 1, year);
 		}
-		else if (month == DECEMBER && day == 31)
+		else /*if (month == DECEMBER && day == 31)*/ // Always true
 		{
 			result = String.format(dateFormat, 1, 1, year + 1);
 		}
-		else
+		// Unreachable
+		/*else
 		{
-			//Unreachable
 			throw new IllegalStateException();
-		}
+		}*/
 
 		return result;
 	}
@@ -89,9 +94,9 @@ public class DateUtil
 
 	static
 	{
-		months31 = new HashSet<Integer>(Arrays.asList(1, 3, 5, 7, 8, 10));
+		months31 = new HashSet<>(Arrays.asList(1, 3, 5, 7, 8, 10));
 
-		months30 = new HashSet<Integer>(Arrays.asList(4, 6, 9, 11));
+		months30 = new HashSet<>(Arrays.asList(4, 6, 9, 11));
 	}
 
 	private static final String dateFormat = "%d/%d/%d";
